244d102458167ce63e01c96cd6e6c08ceecd7fdc
h4xxel <isaksson.axel@telia.com>
2014-11-29 11:31:50 +0100

    added prototypes for memory functions

--- src/mem.h
+++ src/mem.h
@@ -13,4 +13,11 @@ struct Mem {
 void mem_init();
 void *mem_decode_addr(unsigned int address, int *write);
 
+unsigned int m68k_read_memory_8(unsigned int address);
+unsigned int m68k_read_memory_16(unsigned int address);
+unsigned int m68k_read_memory_32(unsigned int address);
+void m68k_write_memory_8(unsigned int address, unsigned int value);
+void m68k_write_memory_16(unsigned int address, unsigned int value);
+void m68k_write_memory_32(unsigned int address, unsigned int value);
+
 #endif
