6090484497147795f46c89ec31d22e0ab6f2a760
h4xxel <isaksson.axel@telia.com>
2014-11-30 23:32:13 +0100

    fixed mmu data structure elements incorrect ordering

--- osloader/mmu.h
+++ osloader/mmu.h
@@ -13,155 +13,154 @@ enum MmuPageSize {
 	MMU_PAGE_SIZE_16K,
 	MMU_PAGE_SIZE_32K,
 };
-
 typedef struct MmuRegTranslationControl MmuRegTranslationControl;
 struct MmuRegTranslationControl {
-	uint32_t table_indices_d : 4;
-	uint32_t table_indices_c : 4;
-	uint32_t table_indices_b : 4;
-	uint32_t table_indices_a : 4;
-	uint32_t initial_shift : 4;
-	uint32_t page_size : 4;
-	uint32_t function_code_lookup : 1;
-	uint32_t supervisor_root_pointer : 1;
-	uint32_t : 5;
 	uint32_t enable : 1;
+	uint32_t : 5;
+	uint32_t supervisor_root_pointer : 1;
+	uint32_t function_code_lookup : 1;
+	uint32_t page_size : 4;
+	uint32_t initial_shift : 4;
+	uint32_t table_indices_a : 4;
+	uint32_t table_indices_b : 4;
+	uint32_t table_indices_c : 4;
+	uint32_t table_indices_d : 4;
 };
 
 typedef struct MmuRegTransparentTranslation MmuRegTransparentTranslation;
 struct MmuRegTransparentTranslation {
-	uint32_t function_code_mask : 3;
+	uint32_t logical_address_base : 8;
+	uint32_t logical_address_mask : 8;
+	uint32_t enable : 1;
+	uint32_t : 4;
+	uint32_t cache_inhibit : 1;
+	uint32_t read_write : 1;
+	uint32_t read_write_mask : 1;
 	uint32_t : 1;
 	uint32_t function_code_base : 3;
 	uint32_t : 1;
-	uint32_t read_write_mask : 1;
-	uint32_t read_write : 1;
-	uint32_t cache_inhibit : 1;
-	uint32_t : 4;
-	uint32_t enable : 1;
-	uint32_t logical_address_mask : 8;
-	uint32_t logical_address_base : 8;
+	uint32_t function_code_mask : 3;
 };
 
 typedef struct MmuRegStatus MmuRegStatus;
 struct MmuRegStatus {
-	uint16_t number_of_levels : 3;
-	uint16_t : 3;
-	uint16_t transparent : 1;
-	uint16_t : 2;
-	uint16_t modified : 1;
-	uint16_t invalid : 1;
-	uint16_t write_protected : 1;
-	uint16_t : 1;
-	uint16_t supervisor_only : 1;
-	uint16_t limit_violation : 1;
 	uint16_t bus_error : 1;
+	uint16_t limit_violation : 1;
+	uint16_t supervisor_only : 1;
+	uint16_t : 1;
+	uint16_t write_protected : 1;
+	uint16_t invalid : 1;
+	uint16_t modified : 1;
+	uint16_t : 2;
+	uint16_t transparent : 1;
+	uint16_t : 3;
+	uint16_t number_of_levels : 3;
 };
 
 typedef struct MmuRegRootPointer MmuRegRootPointer;
 struct MmuRegRootPointer {
-	uint64_t : 4;
-	uint64_t table_address : 28;
-	uint64_t descriptor_type : 2;
-	uint64_t : 14;
-	uint64_t limit : 15;
 	uint64_t lu : 1;
+	uint64_t limit : 15;
+	uint64_t : 14;
+	uint64_t descriptor_type : 2;
+	uint64_t table_address : 28;
+	uint64_t : 4;
 };
 
 struct MmuDescriptorTableShort {
-	uint32_t descriptor_type : 2;
-	uint32_t write_protected : 1;
-	uint32_t used : 1;
 	uint32_t table_address : 28;
+	uint32_t used : 1;
+	uint32_t write_protected : 1;
+	uint32_t descriptor_type : 2;
 };
 
 struct MmuDescriptorTableLong {
-	uint64_t : 4;
-	uint64_t table_address : 28;
-	uint64_t descriptor_type : 2;
-	uint64_t write_protected : 1;
-	uint64_t used : 1;
-	uint64_t : 4;
-	uint64_t supervisor_only : 1;
-	uint64_t : 7;
-	uint64_t limit : 15;
 	uint64_t lu : 1;
+	uint64_t limit : 15;
+	uint64_t : 7;
+	uint64_t supervisor_only : 1;
+	uint64_t : 4;
+	uint64_t used : 1;
+	uint64_t write_protected : 1;
+	uint64_t descriptor_type : 2;
+	uint64_t table_address : 28;
+	uint64_t : 4;
 };
 
 struct MmuDescriptorEarlyTerminationShort {
-	uint32_t descriptor_type : 2;
-	uint32_t write_protected : 1;
-	uint32_t used : 1;
-	uint32_t modified : 1;
+	uint32_t page_address : 24;
 	uint32_t : 1;
 	uint32_t cache_inhibit : 1;
 	uint32_t : 1;
-	uint32_t page_address : 24;
+	uint32_t modified : 1;
+	uint32_t used : 1;
+	uint32_t write_protected : 1;
+	uint32_t descriptor_type : 2;
 };
 
 struct MmuDescriptorEarlyTerminationLong {
-	uint64_t : 8;
-	uint64_t page_address : 24;
-	uint64_t descriptor_type : 2;
-	uint64_t write_protected : 1;
-	uint64_t used : 1;
-	uint64_t modified : 1;
+	uint64_t lu : 1;
+	uint64_t limit : 15;
+	uint64_t : 7;
+	uint64_t supervisor_only : 1;
 	uint64_t : 1;
 	uint64_t cache_inhibit : 1;
 	uint64_t : 1;
-	uint64_t supervisor_only : 1;
-	uint64_t : 7;
-	uint64_t limit : 15;
-	uint64_t lu : 1;
+	uint64_t modified : 1;
+	uint64_t used : 1;
+	uint64_t write_protected : 1;
+	uint64_t descriptor_type : 2;
+	uint64_t page_address : 24;
+	uint64_t : 8;
 };
 
 struct MmuDescriptorPageShort {
-	uint32_t descriptor_type : 2;
-	uint32_t write_protected : 1;
-	uint32_t used : 1;
-	uint32_t modified : 1;
+	uint32_t page_address : 24;
 	uint32_t : 1;
 	uint32_t cache_inhibit : 1;
 	uint32_t : 1;
-	uint32_t page_address : 24;
+	uint32_t modified : 1;
+	uint32_t used : 1;
+	uint32_t write_protected : 1;
+	uint32_t descriptor_type : 2;
 };
 
 struct MmuDescriptorPageLong {
-	uint64_t : 8;
-	uint64_t page_address : 24;
-	uint64_t descriptor_type : 2;
-	uint64_t write_protected : 1;
-	uint64_t used : 1;
-	uint64_t modified : 1;
+	uint64_t : 16;
+	uint64_t : 7;
+	uint64_t supervisor_only : 1;
 	uint64_t : 1;
 	uint64_t cache_inhibit : 1;
 	uint64_t : 1;
-	uint64_t supervisor_only : 1;
-	uint64_t : 7;
-	uint64_t : 16;
+	uint64_t modified : 1;
+	uint64_t used : 1;
+	uint64_t write_protected : 1;
+	uint64_t descriptor_type : 2;
+	uint64_t page_address : 24;
+	uint64_t : 8;
 };
 
 struct MmuDescriptorInvalidShort {
-	uint32_t descriptor_type : 2;
 	uint32_t : 30;
+	uint32_t descriptor_type : 2;
 };
 
 struct MmuDescriptorInvalidLong {
-	uint64_t : 32;
-	uint64_t descriptor_type : 2;
 	uint64_t : 30;
+	uint64_t descriptor_type : 2;
+	uint64_t : 32;
 };
 
 struct MmuDescriptorIndirectShort {
-	uint32_t descriptor_type : 2;
 	uint32_t descriptor_address : 30;
+	uint32_t descriptor_type : 2;
 };
 
 struct MmuDescriptorIndirectLong {
-	uint64_t : 2;
-	uint32_t descriptor_address : 30;
-	uint64_t descriptor_type : 2;
 	uint64_t : 30;
+	uint64_t descriptor_type : 2;
+	uint32_t descriptor_address : 30;
+	uint64_t : 2;
 };
 
 typedef union MmuDescriptorShort MmuDescriptorShort;
