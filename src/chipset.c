dfb5a5a45e13f8e3000b7d2026a98cb63e9f7ba7
h4xxel <isaksson.axel@telia.com>
2014-12-03 18:26:15 +0100

    overhauled the chipset code

--- src/chipset.c
+++ src/chipset.c
@@ -42,57 +42,83 @@ void chipset_int_set(int int_no, int set_unset) {
 
 
 void chipset_write_io(unsigned int addr, unsigned int data) {
-	addr &= 0xFC;
+	unsigned int port = (addr & 0xFC)/4;
 	//fprintf(stderr, "write_io %u %u\n", addr, data);
 	
-	switch(addr) {
-		case 0x0:
+	switch(port) {
+		case CHIPSET_IO_PORT_INTERRUPT_ENABLE:
 			chipset_int_set(0, data & 1);
 			return;
-		case 0x4:
-			chipset_int_set(CHIPSET_INT_NUM_VGA_VSYNC, 0);
-			return;
-		case 0x8:
-			vga_state.reg.window_x = data & 0x3FF;
-			return;
-		case 0xC:
-			vga_state.reg.window_y = data & 0x3FF;
-			return;
-		case 0x10:
-			vga_state.reg.sprite_x = data & 0x3FF;
-			return;
-		case 0x14:
-			vga_state.reg.sprite_y = data & 0x3FF;
-			return;
-		case 0x20:
-			spi_state.mem_counter = data & 0x3FF;
-			return;
-		case 0x24:
-			spi_state.line_select = data & 0x1F;
-			return;
-		case 0x28:
-			spi_state.reg = data & 0xF;
-			return;
-		case 0x2C:
+		case CHIPSET_IO_PORT_IRQ_ACK_SPI:
 			chipset_int_set(CHIPSET_INT_NUM_SPI_DONE, 0);
 			return;
+		case CHIPSET_IO_PORT_IRQ_ACK_VGA:
+			chipset_int_set(CHIPSET_INT_NUM_VGA_VSYNC, 0);
+			return;
+			
+		case CHIPSET_IO_PORT_VGA_WINDOW_X:
+			vga_state.reg.window_x = data & 0x3FF;
+			return;
+		case CHIPSET_IO_PORT_VGA_WINDOW_Y:
+			vga_state.reg.window_y = data & 0x3FF;
+			return;
+		case CHIPSET_IO_PORT_VGA_SPRITE_X:
+			vga_state.reg.sprite_x = data & 0x3FF;
+			return;
+		case CHIPSET_IO_PORT_VGA_SPRITE_Y:
+			vga_state.reg.sprite_y = data & 0x3FF;
+			return;
+		
+		case CHIPSET_IO_PORT_SPI_MEM_COUNTER:
+			spi_state.mem_counter = data & 0x3FF;
+			return;
+		case CHIPSET_IO_PORT_SPI_LINE_SELECT:
+			spi_state.line_select = data & 0x1F;
+			return;
+		case CHIPSET_IO_PORT_SPI_REG:
+			spi_state.reg = data & 0xF;
+			return;
+		
+		default:
+			fprintf(stderr, "Wrote invalid IO-port 0x%X\n", addr);
 	}
-
-	fprintf(stderr, "Wrote invalid IO-port 0x%X\n", addr);
 
 	return;
 }
 
 
 uint32_t chipset_read_io(unsigned int addr) {
-	addr &= 0xFC;
+	unsigned int port = (addr & 0xFC)/4;
 	
-	if (addr == 0x24) {
-		return spi_state.line_select & 0x1F;
-	} if (addr == 0x28) {
-		return spi_state.reg & 0xF;
+	switch(port) {
+		/*
+		case CHIPSET_IO_PORT_NUMBER_INTERRUPT_ENABLE:
+			return 0x0;
+		case CHIPSET_IO_PORT_NUMBER_IRQ_ACK_SPI:
+			return 0x0;
+		case CHIPSET_IO_PORT_NUMBER_IRQ_ACK_VGA:
+			return 0x0;
+		*/
+			
+		case CHIPSET_IO_PORT_VGA_WINDOW_X:
+			return vga_state.reg.window_x  & 0x3FF;
+		case CHIPSET_IO_PORT_VGA_WINDOW_Y:
+			return vga_state.reg.window_y & 0x3FF;
+		case CHIPSET_IO_PORT_VGA_SPRITE_X:
+			return vga_state.reg.sprite_x & 0x3FF;
+		case CHIPSET_IO_PORT_VGA_SPRITE_Y:
+			return vga_state.reg.sprite_y & 0x3FF;
+		
+		case CHIPSET_IO_PORT_SPI_MEM_COUNTER:
+			return spi_state.mem_counter & 0x3FF;
+			return;
+		case CHIPSET_IO_PORT_SPI_LINE_SELECT:
+			return spi_state.line_select & 0x1F;
+		case CHIPSET_IO_PORT_SPI_REG:
+			return spi_state.reg & 0xF;
+		
+		default:
+			fprintf(stderr, "Read invalid IO-port 0x%X\n", addr);	
 	}
-
-	fprintf(stderr, "Read invalid IO-port 0x%X\n", addr);
 	return 0xFFFFFFFF;
 }
