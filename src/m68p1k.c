d1ceda26bfe616cf60890d2722c79069772fb4f4
Axel Isaksson <isaksson.axel@telia.com>
2016-12-07 20:49:27 +0100

    select 030

--- src/m68p1k.c
+++ src/m68p1k.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <pthread.h>
-#include "cpu/m68000.h"
+#include "cpu/m68krash.h"
 #include "mem.h"
 #include "vga.h"
 #include "signal.h"
@@ -16,7 +16,7 @@ void die(int arne) {
 }
 
 static void *run_cpu(void *data) {
-	m68030_start();
+	m68krash_start();
 	
 	pthread_exit(NULL);
 }
@@ -37,8 +37,8 @@ int main(int argc, char **argv) {
 		spi_sd_init(argv[2]);
 	mem_init(argv[1]);
 	vga_init();
-	m68030_init();
-	m68030_reset(true);
+	m68krash_init(M68K_CPU_030);
+	m68krash_reset(true);
 	
 	pthread_attr_init(&attr);
 	pthread_create(&thread, &attr, run_cpu, NULL);
